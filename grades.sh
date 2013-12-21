ad16da846de959b34f32b5900661fc7a45749d3f
Tom Neiser <tomneiser@ucla.edu>
2013-12-21 14:29:30 +1100

    update

--- grades.sh
+++ grades.sh
@@ -2,14 +2,14 @@
 
 ##  -------------------------------------------------------------------------------
 ##  If you've downloaded the roster as an excel file and entered the grades there, this
-##  script will create formatted files for each lab to allow uploading to MyUCLA.
+##  script will create formatted files for each lab or HW to allow uploading to MyUCLA.
 ##  This script takes a somewhat special "Workbook1.txt" as argument: You can create it
 ##  by copying the gradebook body into a new excel file and deleting the three adjacent
 ##  columns containing students' e-mail, major and section as they will not be required.
 ##  Save as a tab-delimited text file "Workbook1.txt" and move to the same directory as
 ##  this script. The first line will be of the format: UID <tab> "Joe Bruin" <tab> # etc.
 ##  Run this script from a Unix command line, "IP:Directory user$ bash this_script.sh"
-##  For the nth Lab on MyUCLA, upload (n-1).txt
+##  For the nth Lab or HW on MyUCLA, upload (n-1).txt
 ##  -------------------------------------------------------------------------------
 
 OLD_IFS="$IFS"                              # secure default IFS
@@ -21,9 +21,9 @@ IFS=$'\t\r\n' roster=($(cat grades.txt))    # create array while ignoring spaces
 
 echo "How many students do you have?"
 read students
-echo "How many labs were completed?"
+echo "How many HWs or Labs were completed?"
 read labN
-echo "OK. Creating files."
+echo "Creating files."
 
 c=$labN+2                                   # number of colums in grade array
 
